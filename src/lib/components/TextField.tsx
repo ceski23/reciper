305ee3a29980c9f1147a300e4360cae6f838e5ab
Cezary Bober <ceski23@gmail.com>
2023-11-11 11:59:45 +0100

    feat: add TextField

---
+++ src/lib/components/TextField.tsx
@@ -0,0 +1,269 @@
+import { styled } from '@macaron-css/react'
+import { type ComponentProps, forwardRef, type ReactNode, useId } from 'react'
+import { styleUtils, theme } from 'lib/styles'
+import { IconButton } from './IconButton'
+import { bodyLarge, bodySmall, Typography } from './Typography'
+import Icon, { type SvgName } from '~virtual/svg-component'
+
+type TextFieldProps = {
+	label: string
+	placeholder?: string
+	value: string
+	onValueChange: (value: string) => void
+	error?: string
+	disabled?: boolean
+	required?: boolean
+	leadingIcon?: SvgName
+	trailingAddon?: ReactNode
+	inputProps?: ComponentProps<'input'>
+}
+
+export const TextField = forwardRef<HTMLInputElement, TextFieldProps>(({
+	label,
+	placeholder,
+	value,
+	onValueChange,
+	error,
+	disabled,
+	required,
+	leadingIcon,
+	trailingAddon,
+	inputProps,
+}, ref) => {
+	const fieldId = useId()
+	const supportingTextId = useId()
+	const hasError = error !== undefined
+	const shouldShrinkLabel = placeholder !== undefined || value.length > 0
+
+	const renderTrailingAddon = (
+		<FieldAddon className={bodyLarge}>
+			{trailingAddon}
+		</FieldAddon>
+	)
+	const renderErrorAddon = <ErrorIcon name="error" />
+	const renderClearAddon = (
+		<ClearButton
+			title="Clear"
+			icon="cancel"
+			onClick={() => onValueChange('')}
+		/>
+	)
+
+	return (
+		<FieldContainer>
+			<FieldInnerContainer
+				hasError={hasError}
+				disabled={disabled}
+			>
+				{leadingIcon && (
+					<FieldAddon className={bodyLarge}>
+						<LeadingIcon name={leadingIcon} />
+					</FieldAddon>
+				)}
+				<FieldInput
+					ref={ref}
+					value={value}
+					onChange={event => onValueChange(event.currentTarget.value)}
+					placeholder={placeholder}
+					id={fieldId}
+					disabled={disabled}
+					aria-required={required}
+					withIcon={leadingIcon !== undefined}
+					className={bodyLarge}
+					aria-invalid={error !== undefined}
+					aria-errormessage={error !== undefined ? supportingTextId : undefined}
+					{...inputProps}
+				/>
+				{trailingAddon
+					? renderTrailingAddon
+					: error
+					? renderErrorAddon
+					: value.length > 0 && !disabled
+					? renderClearAddon
+					: null}
+				<FieldLabel
+					shrinked={shouldShrinkLabel}
+					htmlFor={fieldId}
+					withIcon={leadingIcon !== undefined}
+					className={shouldShrinkLabel ? bodySmall : bodyLarge}
+				>
+					{label}
+					{required && <RequiredIndicator>*</RequiredIndicator>}
+				</FieldLabel>
+			</FieldInnerContainer>
+			{hasError && (
+				<SupportingText
+					id={supportingTextId}
+					hasError={hasError}
+				>
+					{error}
+				</SupportingText>
+			)}
+		</FieldContainer>
+	)
+})
+
+const FieldContainer = styled('div', {
+	base: {
+		display: 'flex',
+		flexDirection: 'column',
+	},
+})
+
+const FieldInnerContainer = styled('div', {
+	base: {
+		display: 'flex',
+		flexDirection: 'row',
+		border: `1px solid ${theme.colors.outline}`,
+		color: theme.colors.onSurfaceVariant,
+		alignItems: 'center',
+		position: 'relative',
+		borderRadius: 4,
+		transition: 'border-color .2s, outline .2s',
+		outline: `2px solid transparent`,
+		outlineOffset: -2,
+		':hover': {
+			borderColor: theme.colors.onSurface,
+		},
+		':focus-within': {
+			outlineColor: theme.colors.primary,
+			borderColor: theme.colors.primary,
+		},
+	},
+	variants: {
+		hasError: {
+			true: {
+				outlineColor: theme.colors.error,
+				borderColor: theme.colors.error,
+				color: theme.colors.error,
+			},
+		},
+		disabled: {
+			true: {
+				color: styleUtils.transparentize(theme.colors.onSurface, 0.5),
+				opacity: 0.38,
+				':hover': {
+					borderColor: theme.colors.outline,
+				},
+			},
+		},
+	},
+})
+
+const FieldInput = styled('input', {
+	base: {
+		color: theme.colors.onSurfaceVariant,
+		border: 'none',
+		background: 'none',
+		paddingBlock: 8,
+		paddingLeft: 16,
+		height: 54,
+		outline: 'none',
+		minWidth: 0,
+		flex: 1,
+		'::placeholder': {
+			color: theme.colors.onSurfaceVariant,
+		},
+	},
+	variants: {
+		withIcon: {
+			true: {
+				paddingLeft: 0,
+			},
+		},
+	},
+})
+
+const FieldLabel = styled('label', {
+	base: {
+		position: 'absolute',
+		transition: 'top .2s, left .2s, font-size .2s, padding-inline .2s, color .2s',
+		top: 15,
+		left: 16,
+		paddingInline: 0,
+		selectors: {
+			':focus-within ~ &': {
+				paddingInline: 4,
+				left: 12,
+				top: -8,
+				backgroundColor: theme.colors.surface,
+				color: theme.colors.primary,
+				// Body/Small
+				fontSize: 12,
+				lineHeight: '16px',
+				letterSpacing: 0.4,
+			},
+		},
+	},
+	variants: {
+		withIcon: {
+			true: {
+				left: 50,
+			},
+		},
+		shrinked: {
+			true: {
+				paddingInline: 4,
+				left: 12,
+				top: -8,
+				backgroundColor: theme.colors.surface,
+			},
+		},
+	},
+})
+
+const ClearButton = styled(IconButton, {
+	base: {
+		marginRight: 8,
+	},
+})
+
+const SupportingText = styled(Typography.BodySmall, {
+	base: {
+		color: theme.colors.onSurfaceVariant,
+		paddingTop: 4,
+		paddingInline: 16,
+	},
+	variants: {
+		hasError: {
+			true: {
+				color: theme.colors.error,
+			},
+		},
+	},
+})
+
+const ErrorIcon = styled(Icon, {
+	base: {
+		width: 40,
+		height: 40,
+		padding: 8,
+		margin: 4,
+	},
+})
+
+const RequiredIndicator = styled('sup', {
+	base: {
+		color: theme.colors.error,
+		marginLeft: 2,
+		lineHeight: 1,
+		fontSize: '70%',
+	},
+})
+
+const LeadingIcon = styled(Icon, {
+	base: {
+		width: 24,
+		height: 24,
+	},
+})
+
+const FieldAddon = styled('div', {
+	base: {
+		minWidth: 40,
+		height: 40,
+		padding: 8,
+		margin: 4,
+		color: theme.colors.onSurfaceVariant,
+	},
+})
--- svg-component.d.ts
+++ svg-component.d.ts
@@ -1,7 +1,7 @@
 
 declare module '~virtual/svg-component' {
-  const Icon: ({name, className, style}: {name: "account" | "animation" | "backArrow" | "brightness" | "close" | "fileDownload" | "fileUpload" | "home" | "info" | "link" | "more" | "numbers" | "palette" | "plus" | "recipes" | "scale" | "search" | "settings" | "sync", className?: string, style?: React.CSSProperties})=> JSX.Element;
-  export const svgNames: ["account" , "animation" , "backArrow" , "brightness" , "close" , "fileDownload" , "fileUpload" , "home" , "info" , "link" , "more" , "numbers" , "palette" , "plus" , "recipes" , "scale" , "search" , "settings" , "sync"];
-  export type SvgName = "account" | "animation" | "backArrow" | "brightness" | "close" | "fileDownload" | "fileUpload" | "home" | "info" | "link" | "more" | "numbers" | "palette" | "plus" | "recipes" | "scale" | "search" | "settings" | "sync";
+  const Icon: ({name, className, style}: {name: "account" | "animation" | "backArrow" | "brightness" | "cancel" | "close" | "error" | "fileDownload" | "fileUpload" | "home" | "info" | "link" | "more" | "numbers" | "palette" | "plus" | "recipes" | "scale" | "search" | "settings" | "sync", className?: string, style?: React.CSSProperties})=> JSX.Element;
+  export const svgNames: ["account" , "animation" , "backArrow" , "brightness" , "cancel" , "close" , "error" , "fileDownload" , "fileUpload" , "home" , "info" , "link" , "more" , "numbers" , "palette" , "plus" , "recipes" , "scale" , "search" , "settings" , "sync"];
+  export type SvgName = "account" | "animation" | "backArrow" | "brightness" | "cancel" | "close" | "error" | "fileDownload" | "fileUpload" | "home" | "info" | "link" | "more" | "numbers" | "palette" | "plus" | "recipes" | "scale" | "search" | "settings" | "sync";
   export default Icon;
 }
