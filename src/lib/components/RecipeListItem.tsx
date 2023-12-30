94bb215f9ed0c371bd16376e86d659c5f0f55c43
Cezary Bober <ceski23@gmail.com>
2023-12-30 17:36:35 +0100

    fix: disable animation causing infinite loop

--- src/lib/components/RecipeListItem.tsx
+++ src/lib/components/RecipeListItem.tsx
@@ -1,21 +1,22 @@
 import { styled } from '@macaron-css/react'
-import { animated, useInView, useSpring } from '@react-spring/web'
+import { animated, useInView } from '@react-spring/web'
 import chickenSoup from 'assets/images/chicken_soup.jpg'
 import { PATHS } from 'lib/routing/paths'
 import { ListItem } from './list/items'
 
 export const RecipeListItem = () => {
-	const [ref, inView] = useInView()
-	const style = useSpring({
-		to: {
-			opacity: inView ? 1 : 0,
-			y: inView ? 0 : 50,
-		},
-	})
+	const [ref /* inView */] = useInView()
+	// TODO: Fix inifinite loop
+	// const style = useSpring({
+	// 	to: {
+	// 		opacity: inView ? 1 : 0,
+	// 		y: inView ? 0 : 50,
+	// 	},
+	// })
 
 	return (
 		<AnimatedListItem
-			style={style}
+			// style={style}
 			ref={ref}
 			overline="Kwestia smaku"
 			title="Sunday chicken soup"
