cc6e970c97439fe96b06ce9abb559d8f3bdbc0d3
Cezary Bober <ceski23@gmail.com>
2024-01-19 20:29:03 +0100

    fix: replace CloseWatcher with useBlocker

--- src/features/recipes/components/Gallery.tsx
+++ src/features/recipes/components/Gallery.tsx
@@ -3,8 +3,9 @@ import { styled } from '@macaron-css/react'
 import * as RovingFocusGroup from '@radix-ui/react-roving-focus'
 import type PhotoSwipe from 'photoswipe'
 import { type EventCallback } from 'photoswipe'
-import { type FunctionComponent, useCallback, useEffect, useRef } from 'react'
+import { type FunctionComponent, useCallback, useRef, useState } from 'react'
 import { Gallery as PhotoSwipeGallery } from 'react-photoswipe-gallery'
+import { useBlocker } from 'react-router-dom'
 import { GalleryItem } from './GalleryItem'
 
 type GalleryProps = {
@@ -13,28 +14,40 @@ type GalleryProps = {
 
 export const Gallery: FunctionComponent<GalleryProps> = ({ images }) => {
 	const galleryRef = useRef<PhotoSwipe>()
+	const [isGalleryOpen, setIsGalleryOpen] = useState(false)
+	const blocker = useBlocker(isGalleryOpen)
 
 	const handleGalleryOpen = useCallback((photoswipe: PhotoSwipe) => {
-		const closeWatcher = new CloseWatcher()
-		closeWatcher.addEventListener('close', () => photoswipe.close(), { once: true })
+		// const closeWatcher = new CloseWatcher()
+		// closeWatcher.addEventListener('close', () => photoswipe.close(), { once: true })
+		setIsGalleryOpen(true)
 
 		const handleGalleryClose: EventCallback<'closingAnimationStart'> = () => {
 			photoswipe.off('closingAnimationStart', handleGalleryClose)
-			closeWatcher.destroy()
+			// closeWatcher.destroy()
+			setIsGalleryOpen(false)
 		}
 
 		photoswipe.on('closingAnimationStart', handleGalleryClose)
 	}, [])
 
-	useEffect(() => {
-		return () => galleryRef.current?.destroy()
-	}, [])
+	// useEffect(() => {
+	// 	return () => galleryRef.current?.destroy()
+	// }, [])
+
+	if (blocker.state === 'blocked') {
+		queueMicrotask(() => {
+			galleryRef.current?.close()
+			blocker.reset()
+		})
+	}
 
 	return (
 		<PhotoSwipeGallery
 			plugins={photoswipe => galleryRef.current = photoswipe.pswp}
 			options={{ escKey: !('CloseWatcher' in globalThis) }}
-			onOpen={'CloseWatcher' in globalThis ? handleGalleryOpen : undefined}
+			// onOpen={'CloseWatcher' in globalThis ? handleGalleryOpen : undefined}
+			onOpen={handleGalleryOpen}
 		>
 			<ImagesGrid>
 				{images.map(image => (
