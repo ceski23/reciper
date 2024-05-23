f4e5a19f8c6426c29f4f5b45fb1c2a65d2e98b51
Cezary Bober <ceski23@gmail.com>
2024-05-23 15:10:31 +0200

    fix: fix types

--- src/lib/components/dialog/withDialogAnimation.tsx
+++ src/lib/components/dialog/withDialogAnimation.tsx
@@ -5,18 +5,14 @@ import { DIALOG_ANIMATION } from 'lib/components/dialog/constants'
 export const withDialogAnimation = <TProps extends { styles?: Record<string, SpringValue<number>>; open?: boolean }>(
 	Component: FunctionComponent<TProps>,
 ) =>
-({
-	open,
-	...props
-}: TProps) => {
-	const transition = useTransition(open, DIALOG_ANIMATION)
+(props: TProps) => {
+	const transition = useTransition(props.open, DIALOG_ANIMATION)
 
 	return transition((styles, open) => (
-		// @ts-expect-error I can't into TypeScript :(
 		<Component
-			styles={styles}
-			open={open}
 			{...props}
+			open={open}
+			styles={styles}
 		/>
 	))
 }
