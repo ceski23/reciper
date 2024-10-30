df98890a7a6813914bea12df16948a52f9c0c5ca
Cezary Bober <ceski23@gmail.com>
2024-10-30 22:46:25 +0100

    chore: update packages

--- src/routes/recipes_/$id.edit.tsx
+++ src/routes/recipes_/$id.edit.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { EditRecipe } from 'features/recipes/EditRecipe'
 
-export const Route = createFileRoute('/recipes/$id/edit')({
+export const Route = createFileRoute('/recipes_/$id/edit')({
 	component: EditRecipe,
 })
